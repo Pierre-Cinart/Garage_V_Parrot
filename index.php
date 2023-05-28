ba71f4cc336498259cedd5dc29c534b790c01447
Pierre-Cinart <cinartpierre@gmail.com>
2023-05-28 22:07:20 +0200

    mise place de l élément footer php et correction des liens réseaux

--- index.php
+++ index.php
@@ -5,11 +5,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" href=".Css/style.css">
+    <link rel="stylesheet" href="./Css/style.css">
     <title>Garage V Parrot</title>
 </head>
 <body>
     <h1>Garage V Parrot</h1>
+    <section class="main"></section>
+    <footer>
+        <?php include './phpComponents/footer.php'; ?>
+    </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
     <script type ="text/javascript"src="./Js/app.js"></script>
 </body>
---
+++ phpComponents/footer.php
@@ -0,0 +1,32 @@
+<div class="row " class = "_footer">
+    <div class="col-3">
+        <p>ECF , création d' un  site web pour un  garage fictif,
+        " réalisé dans le cadre de ma formation de développeur web et d'application web chez Studi  
+        pour la section décembre 2023. </p> 
+    </div>
+    <div class="col-3">
+        <p class = "_footer-contact">
+            Garage V-Parrot , <span>Entretiens , réparations et ventes automobiles </span> 
+            <br>
+            Adresse : 123 rue des Jardins, 75000 Paris, France
+            <br>
+            Téléphone : +33 1 23 45 67 89
+            <br>
+            Email : contact@lesjardinsdedemain.com 
+            </p>
+    </div>
+    <div class="col-6 text-center">
+        ' <div class="box-logos "> 
+            <a href="https://fr-fr.facebook.com/" target="_blank">
+                <img src="./Images/logo_facebook.png" alt="logo_facebook">
+            </a>
+            <a href="https://www.instagram.com/" target="_blank">
+               <img src="./Images/logo_insta.png" alt="logo_insta">
+           </a>
+            <a   href="https://twitter.com/accueil?lang=fr" target="_blank">
+               <img src="./Images/logo_twitter.png" alt="logo_twitter">
+           </a>
+        </div>
+    </div>
+</div>
+<p class="text-center rights">cinartDev tout droits réservés</p>
